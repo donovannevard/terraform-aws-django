6270d07daba1f9aaaff615caf11f575e825ca632
Donovan Nevard <donovan@nevard.dev>
2025-12-31 12:43:22 +0000

    updated code base through testing

--- modules/acm/main.tf
+++ modules/acm/main.tf
@@ -7,11 +7,11 @@ terraform {
   }
 }
 
-# Certificate for ALB - in the same region as everything else (eu-west-2 by default)
+# Certificate for ALB (eu-west-2 default region)
 resource "aws_acm_certificate" "alb" {
   domain_name               = var.domain_name
   subject_alternative_names = var.alternative_names
-  validation_method         = "DNS"
+  validation_method         = "EMAIL"  # Email validation - no DNS records needed
 
   tags = merge(var.tags, {
     Name = "alb-cert"
@@ -23,28 +23,13 @@ resource "aws_acm_certificate" "alb" {
   }
 }
 
-# DNS validation records for ALB cert
-resource "aws_route53_record" "alb_validation" {
-  zone_id = module.route53.zone_id   # ← reference the output from route53 module
-  name    = aws_acm_certificate.alb.domain_validation_options[0].resource_record_name
-  type    = aws_acm_certificate.alb.domain_validation_options[0].resource_record_type
-  records = [aws_acm_certificate.alb.domain_validation_options[0].resource_record_value]
-  ttl     = 60
-}
-
-# Wait for ALB cert validation
-resource "aws_acm_certificate_validation" "alb" {
-  certificate_arn         = aws_acm_certificate.alb.arn
-  validation_record_fqdns = [for record in aws_route53_record.alb_validation : record.fqdn]
-}
-
-# Certificate for CloudFront - MUST be in us-east-1
+# Certificate for CloudFront (must be in us-east-1)
 resource "aws_acm_certificate" "cloudfront" {
-  provider = aws.us_east_1  # Uses the alias provider from root
+  provider = aws.us_east_1
 
   domain_name               = var.domain_name
   subject_alternative_names = var.alternative_names
-  validation_method         = "DNS"
+  validation_method         = "EMAIL"  # Email validation
 
   tags = merge(var.tags, {
     Name = "cloudfront-cert"
@@ -54,21 +39,4 @@ resource "aws_acm_certificate" "cloudfront" {
   lifecycle {
     create_before_destroy = true
   }
-}
-
-# DNS validation records for CloudFront cert (same zone, reusable)
-resource "aws_route53_record" "cloudfront_validation" {
-  zone_id = module.route53.zone_id
-  name    = aws_acm_certificate.cloudfront.domain_validation_options[0].resource_record_name
-  type    = aws_acm_certificate.cloudfront.domain_validation_options[0].resource_record_type
-  records = [aws_acm_certificate.cloudfront.domain_validation_options[0].resource_record_value]
-  ttl     = 60
-}
-
-# Wait for CloudFront cert validation
-resource "aws_acm_certificate_validation" "cloudfront" {
-  provider = aws.us_east_1
-
-  certificate_arn         = aws_acm_certificate.cloudfront.arn
-  validation_record_fqdns = [for record in aws_route53_record.cloudfront_validation : record.fqdn]
 }
--- modules/route53/main.tf
+++ modules/route53/main.tf
@@ -12,70 +12,70 @@ locals {
   cloudfront_zone_id = "Z2FDTNDATAQYW2"  # Fixed global CloudFront zone ID
 }
 
-# A record: apex -> CloudFront
-resource "aws_route53_record" "apex" {
-  zone_id = local.zone_id
-  name    = var.domain_name
-  type    = "A"
+# # A record: apex -> CloudFront
+# resource "aws_route53_record" "apex" {
+#   zone_id = local.zone_id
+#   name    = var.domain_name
+#   type    = "A"
 
-  alias {
-    name                   = var.cloudfront_domain
-    zone_id                = local.cloudfront_zone_id
-    evaluate_target_health = false
-  }
+#   alias {
+#     name                   = var.cloudfront_domain
+#     zone_id                = local.cloudfront_zone_id
+#     evaluate_target_health = false
+#   }
 
-  lifecycle {
-    ignore_changes = [alias]
-  }
-}
+#   lifecycle {
+#     ignore_changes = [alias]
+#   }
+# }
 
-# AAAA record: apex -> CloudFront
-resource "aws_route53_record" "apex_ipv6" {
-  zone_id = local.zone_id
-  name    = var.domain_name
-  type    = "AAAA"
+# # AAAA record: apex -> CloudFront
+# resource "aws_route53_record" "apex_ipv6" {
+#   zone_id = local.zone_id
+#   name    = var.domain_name
+#   type    = "AAAA"
 
-  alias {
-    name                   = var.cloudfront_domain
-    zone_id                = local.cloudfront_zone_id
-    evaluate_target_health = false
-  }
+#   alias {
+#     name                   = var.cloudfront_domain
+#     zone_id                = local.cloudfront_zone_id
+#     evaluate_target_health = false
+#   }
 
-  lifecycle {
-    ignore_changes = [alias]
-  }
-}
+#   lifecycle {
+#     ignore_changes = [alias]
+#   }
+# }
 
-# A record: www -> CloudFront
-resource "aws_route53_record" "www" {
-  zone_id = local.zone_id
-  name    = "www.${var.domain_name}"
-  type    = "A"
+# # A record: www -> CloudFront
+# resource "aws_route53_record" "www" {
+#   zone_id = local.zone_id
+#   name    = "www.${var.domain_name}"
+#   type    = "A"
 
-  alias {
-    name                   = var.cloudfront_domain
-    zone_id                = local.cloudfront_zone_id
-    evaluate_target_health = false
-  }
+#   alias {
+#     name                   = var.cloudfront_domain
+#     zone_id                = local.cloudfront_zone_id
+#     evaluate_target_health = false
+#   }
 
-  lifecycle {
-    ignore_changes = [alias]
-  }
-}
+#   lifecycle {
+#     ignore_changes = [alias]
+#   }
+# }
 
-# AAAA record: www -> CloudFront
-resource "aws_route53_record" "www_ipv6" {
-  zone_id = local.zone_id
-  name    = "www.${var.domain_name}"
-  type    = "AAAA"
+# # AAAA record: www -> CloudFront
+# resource "aws_route53_record" "www_ipv6" {
+#   zone_id = local.zone_id
+#   name    = "www.${var.domain_name}"
+#   type    = "AAAA"
 
-  alias {
-    name                   = var.cloudfront_domain
-    zone_id                = local.cloudfront_zone_id
-    evaluate_target_health = false
-  }
+#   alias {
+#     name                   = var.cloudfront_domain
+#     zone_id                = local.cloudfront_zone_id
+#     evaluate_target_health = false
+#   }
 
-  lifecycle {
-    ignore_changes = [alias]
-  }
-}
+#   lifecycle {
+#     ignore_changes = [alias]
+#   }
+# }
