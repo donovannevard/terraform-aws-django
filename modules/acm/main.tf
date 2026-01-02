d97342d7a60c843eb574b356b22fb9298c5d2dc8
Donovan Nevard <donovan@nevard.dev>
2026-01-02 10:02:21 +0000

    updated code base through testing

--- modules/acm/main.tf
+++ modules/acm/main.tf
@@ -2,7 +2,7 @@
 resource "aws_acm_certificate" "alb" {
   domain_name               = var.domain_name
   subject_alternative_names = var.alternative_names
-  validation_method         = "DNS"
+  validation_method         = "EMAIL"
 
   tags = merge(var.tags, {
     Name = "alb-cert"
@@ -15,26 +15,26 @@ resource "aws_acm_certificate" "alb" {
 }
 
 # DNS validation records for ALB cert
-resource "aws_route53_record" "alb_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.alb.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      type   = dvo.resource_record_type
-      value  = dvo.resource_record_value
-    }
-  }
+# resource "aws_route53_record" "alb_validation" {
+#   for_each = {
+#     for dvo in aws_acm_certificate.alb.domain_validation_options : dvo.domain_name => {
+#       name   = dvo.resource_record_name
+#       type   = dvo.resource_record_type
+#       value  = dvo.resource_record_value
+#     }
+#   }
 
-  zone_id = var.hosted_zone_id
-  name    = each.value.name
-  type    = each.value.type
-  records = [each.value.value]
-  ttl     = 60
-}
+#   zone_id = var.hosted_zone_id
+#   name    = each.value.name
+#   type    = each.value.type
+#   records = [each.value.value]
+#   ttl     = 60
+# }
 
-resource "aws_acm_certificate_validation" "alb" {
-  certificate_arn         = aws_acm_certificate.alb.arn
-  validation_record_fqdns = [for record in aws_route53_record.alb_validation : record.fqdn]
-}
+# resource "aws_acm_certificate_validation" "alb" {
+#   certificate_arn         = aws_acm_certificate.alb.arn
+#   validation_record_fqdns = [for record in aws_route53_record.alb_validation : record.fqdn]
+# }
 
 # Certificate for CloudFront (us-east-1)
 resource "aws_acm_certificate" "cloudfront" {
@@ -42,7 +42,7 @@ resource "aws_acm_certificate" "cloudfront" {
 
   domain_name               = var.domain_name
   subject_alternative_names = var.alternative_names
-  validation_method         = "DNS"
+  validation_method         = "EMAIL"
 
   tags = merge(var.tags, {
     Name = "cloudfront-cert"
@@ -55,25 +55,25 @@ resource "aws_acm_certificate" "cloudfront" {
 }
 
 # DNS validation records for CloudFront cert
-resource "aws_route53_record" "cloudfront_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.cloudfront.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      type   = dvo.resource_record_type
-      value  = dvo.resource_record_value
-    }
-  }
+# resource "aws_route53_record" "cloudfront_validation" {
+#   for_each = {
+#     for dvo in aws_acm_certificate.cloudfront.domain_validation_options : dvo.domain_name => {
+#       name   = dvo.resource_record_name
+#       type   = dvo.resource_record_type
+#       value  = dvo.resource_record_value
+#     }
+#   }
 
-  zone_id = var.hosted_zone_id
-  name    = each.value.name
-  type    = each.value.type
-  records = [each.value.value]
-  ttl     = 60
-}
+#   zone_id = var.hosted_zone_id
+#   name    = each.value.name
+#   type    = each.value.type
+#   records = [each.value.value]
+#   ttl     = 60
+# }
 
-resource "aws_acm_certificate_validation" "cloudfront" {
-  provider = aws.us_east_1
+# resource "aws_acm_certificate_validation" "cloudfront" {
+#   provider = aws.us_east_1
 
-  certificate_arn         = aws_acm_certificate.cloudfront.arn
-  validation_record_fqdns = [for record in aws_route53_record.cloudfront_validation : record.fqdn]
-}
+#   certificate_arn         = aws_acm_certificate.cloudfront.arn
+#   validation_record_fqdns = [for record in aws_route53_record.cloudfront_validation : record.fqdn]
+# }
